b3acf87bd85ef830fc6ab34d1f94cc401c383eed
Chander Mohan <130903788+chanduo5@users.noreply.github.com>
2025-02-16 17:55:23 +0530

    Add files via upload

---
+++ Division/code.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input("enter a:"))
+    b = int(input("enter b:"))
+    
+    division_int = a / b
+    division_float = a // b
+    print( division_float)
+    print(division_int)
+    
+    
